e69db45c902abfa61df8fc460066a9f407fdb172
Saydia <sadiatasnim646@gmail.com>
2026-09-15 18:44:59 +0600

    first commit

---
+++ src/app/docs/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DocsPage = () => {
+    return (
+        <div>
+            <h2>our official documentation</h2>
+        </div>
+    );
+};
+
+export default DocsPage;
---
+++ src/app/showcase/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShowCasePage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ShowCasePage;
